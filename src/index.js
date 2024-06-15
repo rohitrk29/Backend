d096e5c83d2682eefe89348fdd94fff2545311f2
rohitrk29 <149864470+rohitrk29@users.noreply.github.com>
2024-06-15 14:17:39 +0530

    .

---
+++ src/index.js
@@ -0,0 +1,56 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+import express from "express"
+const app = express()
+
+// function connectDB(){}
+
+// connectDB()
+
+
+// using iife (()=>{})() //";" use for cleaning purpose when upper line doesnot have ";". so it wont give error.
+;( async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/{DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERR:", error);
+            throw error
+        })
+
+        app.listen(procexx.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.log("ERROR:", error)
+        throw err
+    }
+})()
+*/    
+    
