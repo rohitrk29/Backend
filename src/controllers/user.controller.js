cd3a586c43b3b380c8440ecb43193e3221d0df1d
rohitrk29 <149864470+rohitrk29@users.noreply.github.com>
2024-06-21 03:02:54 +0530

    .

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -20,7 +20,7 @@ const registerUser = asyncHandler( async (req, res) => {
 
 
     const {fullName, email, username, password } = req.body
-    console.log("email:", email);
+    // console.log("email:", email);
 
     // if(fullName === ""){
     //     throw new ApiError(400, "fullname is required")
