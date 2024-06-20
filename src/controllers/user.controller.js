8603e5cc98ea3b820a5361577980cc9f7ce5e5ea
rohitrk29 <149864470+rohitrk29@users.noreply.github.com>
2024-06-21 01:10:04 +0530

    postman api hit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -27,14 +27,14 @@ const registerUser = asyncHandler( async (req, res) => {
     // } (or)
 
     //some method: Determines whether the specified callback function returns true for any element of an array. 
-    if([fullName, email, username, password].some((feild) => 
+    if([fullName, email, username, password].some((field) => 
         field?.trim() === ""))
     {
-        throw new ApiError(400, "All feilds are required")
+        throw new ApiError(400, "All fields are required")
     }
 
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -44,7 +44,12 @@ const registerUser = asyncHandler( async (req, res) => {
 
     // localfilepath return by multer as avatarLocalPath
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // or
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required")
