5bd4629a883341535ac5eee5eb932836ebe5c082
rohitrk29 <149864470+rohitrk29@users.noreply.github.com>
2024-06-20 21:13:52 +0530

    controllers setup

--- src/app.js
+++ src/app.js
@@ -15,4 +15,18 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+
+
+//routes import
+ 
+// we can use any name for import when we are doing default export like using import name as userRouter bcoz export of router as default in ./routes/user.routes.js
+
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+// app.use("/users", userRouter)
+app.use("/api/v1/users", userRouter)
+
+
+// https://localhost:8000/api/v1/users will call userRouter and redirect to user.routes.js file and call register https://localhost:8000/api/v1/users/register
 export { app }
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,6 @@
 // using Promises
 
+// higherorder function return as a function
 const asyncHandler = (requestHandler) => {
     return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
